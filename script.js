d0ff945ddad5b940998d7e2dd88cedc9ae69bfa4
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-09-29 20:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //your JS code here. If required.
-const form = document.getElementById('form');
+const form = document.getElementById('signup-form');
 const submitBtn = document.getElementById('submitBtn');
 form.addEventListener('submit' ,function (event){
 	event.preventDefault();
+	let name = document.getElementById('name').value.trim();
+	let email = document.getElementById('email').value.trim();
 	let pass = document.getElementById('password').value.trim();
 	let confirmPass = document.getElementById('confirm-password').value.trim();
 	if(pass===confirmPass){
+		let data = {
+			    'name': name,
+			    'email': email,
+			    'password': password
+			};
+		localStorage.setItem('data', JSON.stringify(data));
 		alert("Sign up successful!");
 	}
 	else{
