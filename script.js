98eb4f1256d81f8969d2363c7032b0fd5b78aa21
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-09-29 20:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const form = document.getElementById('form');
+const submitBtn = document.getElementById('submitBtn');
+form.addEventListener('submit' ,function (event){
+	event.preventDefault();
+	let pass = document.getElementById('password').value.trim();
+	let confirmPass = document.getElementById('confirm-password').value.trim();
+	if(pass===confirmPass){
+		alert("Sign up successful!");
+	}
+	else{
+		alert("Passwords do not match");
+	}
+})
